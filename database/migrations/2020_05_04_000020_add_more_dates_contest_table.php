27cdcb04e46cdbfb00cf11763fda36ea0b6acc9f
vagrant <vagrant@homestead>
2020-05-06 14:58:28 +0000

    fix de panel

---
+++ database/migrations/2020_05_04_000020_add_more_dates_contest_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\User;
+
+class AddMoreDatesContestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'contests',
+            function (Blueprint $table) {
+                $table->timestamp('votes_end_date')->default(now())->after('end_date');
+                $table->integer('min_apps_qty')->default(10)->after('votes_end_date');
+            }
+        );
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'users',
+            function (Blueprint $table) {
+                $table->dropColumn('votes_end_date');
+                $table->dropColumn('min_apps_qty');
+            }
+        );
+    }
+}
