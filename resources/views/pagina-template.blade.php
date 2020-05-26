7d54cb28953b0f14f61dc80e96c5da0d1df342e6
Manuel Fernández <manuel.fernandez@amplifica.com.ar>
2020-05-26 18:47:46 -0300

    Actualización de textos y estilos

--- resources/views/pagina-template.blade.php
+++ resources/views/pagina-template.blade.php
@@ -2,15 +2,13 @@
 
 
 @section('content')
-    <section class="contenedor intro_gral">
-        <div class="grid_70">
-            <span class="span_h1">{{$pagina->title}}</span>
-            <h1>{{$pagina->copete}}</h1>
-        </div>
-    </section>
 
-    <section class="contenedor">
-        <div class="grid_70 mg_100">
+    <section id="intro" class="contenedor intro_gral ">
+        <div class="titulo tit_term">
+            <h1 class="span_h1">{{$pagina->title}}</h1>
+            <p><strong>{{$pagina->copete}}</strong></p>
+        </div>   
+        <div class="cuerpo_texto">  
             {!! $pagina->texto !!}
         </div>
     </section>
