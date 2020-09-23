f71b7fb3f8e63864226140605d8599c9afbf1235
Manu <manuel.fernandez@amplifica.com.ar>
2020-09-23 14:25:22 -0300

    Modificaciones de mensaje de confirmación de mail + Visualizacion de fichas + Fichas extra por perfil completo (sin campo de descripción)

--- routes/web.php
+++ routes/web.php
@@ -91,6 +91,10 @@ Route::post(
     'WebController@contacto_send'
 )->name("contacto-send");
 
+Route::get(
+    '/restablecer-clave',
+    'WebController@restablecer_clave'
+)->name('restablecer-clave'); 
 
 /* ACCESO RESTRINGIDO */
 Route::middleware(['verified'])->group(
@@ -281,8 +285,7 @@ Route::middleware(['verified'])->group(
             '/novedades/{slug}',
             'ContenidoController@index'
         )->name('novedades');
-
-
+ 
         Route::get(
             '/{slug}',
             'ContenidoController@index'
