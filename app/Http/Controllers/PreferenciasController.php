509236fc783bd6d3ad9e70c951eee14b2c1376bf
matogallardo <rmatiasgallardo@gmail.com>
2020-10-09 01:08:39 -0300

    cambio de zonas con gmt

--- app/Http/Controllers/PreferenciasController.php
+++ app/Http/Controllers/PreferenciasController.php
@@ -19,17 +19,40 @@ class PreferenciasController extends Controller
         $data['idiomas'] = ["Español", "Inglés"];
         $data['monedas'] = ["Peso Argentino (ARS)", "Dólar Americano (USD)", "Euro (EUR)"];
         $data['pagos'] = ["Mercado Pago Argentina", "Paypal"];
-        $data['zonas'] = timezone_identifiers_list();
+        $data['zonas'] = $this->getAllTimeZones();
         return View('preferencias', $data);
+    }
+
+    private function getAllTimeZonesByCountry($country, &$countries)
+    {
+
+        foreach (\DateTimeZone::listIdentifiers($country) as $zoneLabel) {
+            $currentTimeInZone = new \DateTime("now", new \DateTimeZone($zoneLabel));
+            $currentTimeDiff = $currentTimeInZone->format('P');
+            $countries[$zoneLabel] = "(GMT {$currentTimeDiff}) {$zoneLabel}";
+        }
+
+    }
+
+    private function getAllTimeZones()
+    {
+        $countries = [\DateTimeZone::AMERICA, \DateTimeZone::EUROPE, \DateTimeZone::ASIA, \DateTimeZone::AUSTRALIA, \DateTimeZone::AFRICA, \DateTimeZone::INDIAN, \DateTimeZone::PACIFIC];
+        $list = [];
+        foreach ($countries as $country) {
+            $this->getAllTimeZonesByCountry($country, $list);
+        }
+
+        return $list;
+
     }
 
     public function guardar_configuracion_preferencias(Request $request)
     {
         $request->validate([
-           "idioma"=> 'required',
-           "moneda"=> 'required',
-           "pago"=> 'required',
-           "zona"=> 'required',
+            "idioma" => 'required',
+            "moneda" => 'required',
+            "pago" => 'required',
+            "zona" => 'required',
         ]);
         $user = Auth::user();
         $preferencia = PreferenciasModel::firstOrNew(array('user_id' => $user->id));
@@ -39,33 +62,37 @@ class PreferenciasController extends Controller
         $preferencia->zona = $request->zona;
         $preferencia->user_id = $user->id;
         $preferencia->save();
-        echo json_encode(['success'=> true, 'msg'=> 'Preferencias guardadas']);
+        echo json_encode(['success' => true, 'msg' => 'Preferencias guardadas']);
     }
 
-    public function configurar_privacidad() {
+    public function configurar_privacidad()
+    {
         $data = $this->getUserData();
         return View('privacidad', $data);
     }
 
-    public function desactivar_cuenta() {
+    public function desactivar_cuenta()
+    {
         $data = $this->getUserData();
         return View('desactivar-cuenta', $data);
     }
 
-    public function confirmar_desactivar_cuenta() {
+    public function confirmar_desactivar_cuenta()
+    {
         $data = $this->getUserData();
         return View('confirmar-desactivar-cuenta');
     }
 
-    public function borrar_cuenta(Request $request) {
+    public function borrar_cuenta(Request $request)
+    {
         $request->validate(
             ["confirmar" => "required"]
         );
-        if($request->confirmar == "confirmar") {
+        if ($request->confirmar == "confirmar") {
             $user = Auth::user();
             $user->name = "anónimo";
             $user->lastName = "";
-            $user->userName = "anónimo-".uniqid();
+            $user->userName = "anónimo-" . uniqid();
             $user->country = "";
             $user->provincia = "";
             $user->city = "";
@@ -76,7 +103,7 @@ class PreferenciasController extends Controller
             $user->whatsapp = "";
             $user->twitter = "";
             $user->instagram = "";
-            $user->email = "anonymnous".uniqid()."@gmail.com";
+            $user->email = "anonymnous" . uniqid() . "@gmail.com";
             $user->password = password_hash(uniqid(), PASSWORD_DEFAULT);
             $user->avatar = null;
             $user->blocked = 1;
@@ -86,7 +113,8 @@ class PreferenciasController extends Controller
         }
     }
 
-    public function cuenta_desactivada() {
+    public function cuenta_desactivada()
+    {
         return View('cuenta-desactivada');
     }
 }
