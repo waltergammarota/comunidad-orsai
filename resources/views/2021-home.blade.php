dae8115e8befbde9e2d5242b7421d0a647beb3cf
Manuel <manuel.fernandez@amplifica.com.ar>
2021-01-13 12:32:00 -0300

    Mensajes dinámicos módulo SMS

--- resources/views/2021-home.blade.php
+++ resources/views/2021-home.blade.php
@@ -74,33 +74,63 @@
     <section class="resaltado_gris pd_tp_bt pd_20 widt_100">
         <div class="contenedor grilla_3 card_style_4_grid">
 
-            <article class="card_style_4 card_style_4_tit">
-                <h2 class=""><strong>¡Adiós fase beta!</strong><br>Es momento de que certifiques tu identidad, así que
-                    mejor peináte.</h2>
-                <a href="{{url('validacion-usuario')}}" class="link_subrayado mg_20"><span>Los primeros 15.000<br>serán «fundadores»</span></a>
-            </article>
+            @if (!Auth::check()) 
+                <article class="card_style_4 card_style_4_tit">
+                    <h2 class=""><strong>¿Querés ser parte?</strong><br>Participar de Orsai es gratis, divertido y a veces incluso legal.</h2>
+                    <p>Si te apurás serás un <a href="{{url('novedades/socios-fundadores')}}" class="link_subrayado"><span>«socio fundador»</span></a></p>
+                </article>
+            @else
+                @if(Auth::user()->phone_verified_at) 
+                    <article class="card_style_4 card_style_4_tit">
+                        <h2 class=""><strong>¡Mirálo de adentro!</strong><br>Sentate tranqui en la vereda a ver cómo crecen los socios posta.</h2>
+                        <p>Tus derechos como <a href="{{url('novedades/socios-fundadores')}}" class="link_subrayado"><span>«socio fundador»</span></a></p>
+                    </article>
+                @else
+                    <article class="card_style_4 card_style_4_tit">
+                        <h2 class=""><strong>¡Adiós fase beta!</strong><br>Es momento de que certifiques tu identidad, así que
+                            mejor peináte.</h2>
+                        <p>Los primeros 15.000 serán <a href="{{url('novedades/socios-fundadores')}}" class="link_subrayado"><span>«fundadores»</span></a></p> 
+                    </article>
+                @endif
+            @endif  
 
             <article class="card_style_4 ">
                 <div>
                     <span class="icono icon-card"></span>
-                    <h2 class="">{{$sociosBeta}}</h2>
-                    <p class="">socios beta</p>
+                    <h2>{{$sociosBeta}}</h2>
+                    <p>socios beta</p>
                 </div>
             </article>
             <article class="card_style_4">
                 <div>
                     <span class="icono icon-ficha"></span>
-                    <h2 class="">{{$sociosPosta}}</h2>
-                    <p class="">socios posta</p>
+                    <h2>{{$sociosPosta}}</h2>
+                    <p>socios posta</p>
                 </div>
             </article>
 
-        </div>
-        <div class="contenedor cont_bt">
-            <a href="{{url('validacion-usuario')}}" class="boton_redondeado resaltado_amarillo">
-                <span>Confirmá tu membresía</span>
-            </a>
-        </div>
+        </div>  
+        @if (!Auth::check()) 
+            <div class="contenedor cont_bt">
+                <a href="{{url('registrarse')}}" class="boton_redondeado resaltado_amarillo">
+                    <span>Asociáte ahora</span>
+                </a>
+            </div>
+        @else
+            @if(Auth::user()->phone_verified_at) 
+                <div class="contenedor cont_bt">
+                    <a href="{{url('perfil')}}" class="boton_redondeado resaltado_amarillo">
+                        <span>Tu información personal</span>
+                    </a>
+                </div> 
+            @else
+                <div class="contenedor cont_bt">
+                    <a href="{{url('validacion-usuario')}}" class="boton_redondeado resaltado_amarillo">
+                        <span>Confirmá tu membresía</span>
+                    </a>
+                </div>
+            @endif
+        @endif   
     </section>
     <section class="resaltado_black pd_tp_bt pd_20">
         <div class="contenedor titulo_seccion_med">
