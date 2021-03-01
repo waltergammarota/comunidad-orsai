9fb86f2791c6cfb1c20199405e8d4fea10d7309e
Sebastián Miñones <pollaki@flydevs.com>
2021-03-01 09:25:01 -0300

    landing concurso orsai

---
+++ resources/views/fundacion/concursos-inscripcion.blade.php
@@ -0,0 +1,17 @@
+@extends('2021-orsai-template')
+
+@section('title', 'Linea de tiempo Orsai | Comunidad Orsai')
+@section('description','Linea de tiempo Orsai | Comunidad Orsai')
+
+
+@section('content')
+<section class="inscripcion-cuento hero">
+  <div class="contenedor">
+    asdasdasd
+  </div>
+</section>
+@endsection
+ 
+@section('footer')
+  @include("fundacion.footer-fundacion")
+@endsection
