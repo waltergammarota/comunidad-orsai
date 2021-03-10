c3a65b37dc5d695fe7ea4298716edd7a9bdd82ce
Manuel <manuel.fernandez@amplifica.com.ar>
2021-03-10 12:26:48 -0300

    Cambios en mail de Donacion

--- resources/views/mails/detalle_donacion.blade.php
+++ resources/views/mails/detalle_donacion.blade.php
@@ -79,7 +79,7 @@
                                                     <td style="padding:2px 0; border-top:0; border-bottom:0;">Total en Pesos Argentinos
                                                     </td>
                                                     <td style="padding:2px 0; border-top:0; border-bottom:0;text-align: right;">
-                                                        $ {{$amount}}</td>
+                                                        ARS {{$amount}}</td>
                                                 </tr>
                                                 <tr>
                                                     <td style="padding:2px 0; border-top:0; border-bottom:0;">Donante
