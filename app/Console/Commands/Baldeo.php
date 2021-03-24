5ed0bc70f13382ff418369c3cb2c2f4a5efeb17a
matogallardo <rmatiasgallardo@gmail.com>
2021-03-24 10:50:51 -0300

    fix de baldeo

--- app/Console/Commands/Baldeo.php
+++ app/Console/Commands/Baldeo.php
@@ -6,11 +6,12 @@ namespace App\Console\Commands;
 use App\Databases\BaldeoModel;
 use App\Databases\Transaction;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class Baldeo extends Command
 {
-    protected $signature = "baldeo:mes {month} {year}";
+    protected $signature = "baldeo:mes";
 
     protected $description = "makes the baldeo for every user";
 
@@ -21,39 +22,56 @@ class Baldeo extends Command
 
     public function handle()
     {
-        $month = $this->argument("month");
-        $year = $this->argument("year");
-        $this->info("Running baldeo for {$month} - {$year}");
-        User::whereNotNull('email_verified_at')->chunk(2, function ($users) use ($month, $year) {
-            $log = [];
-            foreach ($users as $user) {
-                $balance = $user->getBalance();
-                $amount = round(floor($balance * env("PORCENTAJE_BALDEO", 10) / 100), 0);
-                $poolOrsai = 1;
-                if ($amount > 0) {
-                    Transaction::createTransaction(
-                        $poolOrsai,
-                        $user->id,
-                        $amount,
-                        "Baldeo del mes {$month} - {$year}",
-                        null,
-                        'BURN',
-                        ["baldeo"]);
-                }
-                array_push($log, [
-                    "previous_balance" => $balance,
-                    "user" => $user->id,
-                    "burn_amount" => $amount,
-                    "current_balance" => $user->getBalance()
-                ]);
+        $today = Carbon::now();
+        $month = $today->format('m');
+        $year = $today->format('Y');
+        if (env('BALDEO_ENABLED', FALSE)) {
+            if (!$this->hasBeenRun($month, $year)) {
+                $this->info("Running baldeo for {$month} - {$year}");
+                User::whereNotNull('email_verified_at')->chunk(100, function ($users) use ($month, $year) {
+                    $log = [];
+                    foreach ($users as $user) {
+                        $balance = $user->getBalance();
+                        $amount = round(floor($balance * env("PORCENTAJE_BALDEO", 10) / 100), 0);
+                        $poolOrsai = 1;
+                        if ($amount > 10) {
+                            Transaction::createTransaction(
+                                $user->id,
+                                $poolOrsai,
+                                $amount,
+                                "Baldeo del mes {$month} - {$year}",
+                                null,
+                                'TRANSFER',
+                                ["baldeo: {$month}-{$year}"]);
+                            array_push($log, [
+                                "previous_balance" => $balance,
+                                "user" => $user->id,
+                                "burn_amount" => $amount,
+                                "current_balance" => $user->getBalance(),
+                                "porcentaje_baldeo" => env("PORCENTAJE_BALDEO", 10)
+                            ]);
+                        }
+                    }
+                    $baldeoLog = new BaldeoModel([
+                        "log" => $log,
+                        "type" => "baldeo",
+                        "month" => $month,
+                        "year" => $year
+                    ]);
+                    $baldeoLog->save();
+                });
+            } else {
+                $this->info("Baldeo has been run in {$month} {$year}");
             }
-            $baldeoLog = new BaldeoModel([
-                "log" => $log,
-                "type" => "baldeo",
-                "month" => $month,
-                "year" => $year
-            ]);
-            $baldeoLog->save();
-        });
+        } else {
+            $this->info("Baldeo is not enabled");
+        }
+    }
+
+    private function hasBeenRun($month, $year)
+    {
+        $isAny = BaldeoModel::where("month", $month)->where("year", $year)->count();
+        $this->info("Found: {$isAny}");
+        return $isAny > 0;
     }
 }
--- app/Console/Commands/Mordida.php
+++ app/Console/Commands/Mordida.php
@@ -3,16 +3,15 @@
 
 namespace App\Console\Commands;
 
-use App\Databases\BaldeoModel;
 use App\Databases\Transaction;
 use App\User;
 use Illuminate\Console\Command;
 
-class Baldeo extends Command
+class Mordida extends Command
 {
-    protected $signature = "baldeo:mes {month} {year}";
+    protected $signature = "mordida:dia";
 
-    protected $description = "makes the baldeo for every user";
+    protected $description = "makes the mordida for every user every day";
 
     public function __construct()
     {
@@ -21,43 +20,10 @@ class Baldeo extends Command
 
     public function handle()
     {
-        $month = $this->argument("month");
-        $year = $this->argument("year");
-        if (env('BALDEO_ENABLED', FALSE)) {
-            $this->info("Running baldeo for {$month} - {$year}");
-            User::whereNotNull('email_verified_at')->chunk(100, function ($users) use ($month, $year) {
-                $log = [];
-                foreach ($users as $user) {
-                    $balance = $user->getBalance();
-                    $amount = round(floor($balance * env("PORCENTAJE_BALDEO", 10) / 100), 0);
-                    $poolOrsai = 1;
-                    if ($amount > 0) {
-                        Transaction::createTransaction(
-                            $user->id,
-                            $poolOrsai,
-                            $amount,
-                            "Baldeo del mes {$month} - {$year}",
-                            null,
-                            'TRANSFER',
-                            ["baldeo"]);
-                    }
-                    array_push($log, [
-                        "previous_balance" => $balance,
-                        "user" => $user->id,
-                        "burn_amount" => $amount,
-                        "current_balance" => $user->getBalance()
-                    ]);
-                }
-                $baldeoLog = new BaldeoModel([
-                    "log" => $log,
-                    "type" => "baldeo",
-                    "month" => $month,
-                    "year" => $year
-                ]);
-                $baldeoLog->save();
-            });
-        } else {
-            $this->info("Baldeo is not enabled");
-        }
+        User::whereNotNull('email_verified_at')->chunk(100, function ($users) {
+            foreach ($users as $user) {
+                $txs = Transaction::where('to', $user->id)->where('type', 'MINT')->get();
+            }
+        });
     }
 }
